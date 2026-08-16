b66d149b1311b4f547fab5bed7c25d076bf070ba
ayushmishra34er <ayushmishra34er@gmail.com>
2026-08-16 14:26:13 +0530

    js:created element builder

--- src/app.js
+++ src/app.js
@@ -5,11 +5,43 @@ const DOM = {
 
 }
 
-const terminalInputLine = document.createElement("div");
 
-terminalInputLine.setAttribute('id', 'line-1');
-terminalInputLine.setAttribute('class', 'terminal-text');
-terminalInputLine.textContent = "$>";
 
 DOM.terminalContent.appendChild(terminalInputLine);
 
+function buildElement(parentElement, tagName, text, options = {} ){
+    if(!parentElement) {
+        console.error("Parent element is missing.")
+        return null;
+    }
+
+    const el = document.createElement(tagName);
+    
+    if(text){
+        el.textContent = text;
+
+    }
+
+    if (options.id){
+        el.id = options.id;
+
+    }
+
+    if(options.className){
+        el.className = options.className;
+
+
+    }
+
+    if(options.attributes){
+        for (const[key, value] of Object.entries(options.attributes)){el.setAttribute(key, value)
+
+        }
+    }
+
+    parentElement.appendChild(el);
+    
+
+    return el;
+    
+}
